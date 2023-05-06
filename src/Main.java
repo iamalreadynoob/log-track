37dd0b0adc051f136951b01f48b5340ed5921071
M3R3K <keremab61@gmail.com>
2023-05-06 14:41:42 +0300

    tracking time

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import gui.Screen;
 import track.Tracking;
-
+import fileReading.SavfReading;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.*;
 
 
@@ -8,6 +10,19 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Timer timer = new Timer();
+        SavfReading svf = new SavfReading();
+        svf.scan("./data/settings.savf");
+        Integer frequency = Integer.parseInt(svf.getValue("freq"));
+        frequency = frequency*1000;
+        timer.schedule(new Main.MyTask(), frequency);
+
+        // screen
         new Screen();
+    }
+    static class MyTask extends TimerTask {
+        public void run() {
+            System.out.println("Smtp run");
+        }
     }
 }
